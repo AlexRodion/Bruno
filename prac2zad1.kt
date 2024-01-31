d884efb5247e6e14f3804bea25e43df391fcf631
AlexRodion <128135209+AlexRodion@users.noreply.github.com>
2024-01-31 12:00:07 +0300

    Delete prac2zad1.kt

--- prac2zad1.kt
+++
@@ -1,8 +0,0 @@
-package org.example
-
-fun main() {
-    val amount = 1500000
-    val minCommission = 35
-    val commission = if ((amount * 0.0075) > minCommission) amount * 0.0075 else minCommission
-    println("Комиссия: $commission рублей")
-}
