5ba5f1811059ad30fa7648541d8d262c60de1918
AlexRodion <128135209+AlexRodion@users.noreply.github.com>
2024-01-31 12:00:18 +0300

    Delete prac2zad2.kt

--- prac2zad2.kt
+++
@@ -1,7 +0,0 @@
-package org.example
-
-fun main() {
-    val likes = 111
-    val end = if ((likes % 10 == 1) && (likes % 100 !==11)) "человеку" else "людям"
-    println("Понравилось $likes $end")
-}
